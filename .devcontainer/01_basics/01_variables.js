0139a58ec7da92689d274136c57608d27b84489d
pawandeepkaurq <143918190+pawandeepkaurq@users.noreply.github.com>
2024-04-14 19:43:19 +0000

    config done

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,8 @@ const accountId = 144553
 let accaoutEmail = "pawwannk@gmail.com"
 var accountPassword = "12345"
 accountCity = "jaipur"
-
+ 
 //accountId = 2
 
+accountEmail=""
 console.log("accountId")
